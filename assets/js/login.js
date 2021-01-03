3ed69bde303581a40f6ceb596cfa87cb42152ecd
dali <1175@qq.com>
2021-01-03 17:16:24 +0800

    提交第二次

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,12 @@
-$(function() {
+$(function () {
   // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+  $('#link_reg').on('click', function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
   // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
+  $('#link_login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
@@ -17,9 +17,10 @@ $(function() {
   // 通过 form.verify() 函数自定义校验规则
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
+    // [\S]验证输入不带空格的字符串
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
     // 校验两次密码是否一致的规则
-    repwd: function(value) {
+    repwd: function (value) {
       // 通过形参拿到的是确认密码框中的内容
       // 还需要拿到密码框中的内容
       // 然后进行一次等于的判断
@@ -32,7 +33,7 @@ $(function() {
   })
 
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
+  $('#form_reg').on('submit', function (e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
     // 2. 发起Ajax的POST请求
@@ -40,7 +41,7 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function(res) {
+    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
@@ -50,16 +51,30 @@ $(function() {
     })
   })
 
+
+
+  // $("#form_reg").on('submit', function (e) {
+  //   e.preventDefault()    //取消默认行为
+  //   $.post("http://ajax.frontend.itheima.net/api/reguser", { username: $('#form_reg[name = username]').val(), password: $('#form_reg[name = password]').val() }, function (res) {
+  //     if (res.status !== 0) {
+  //       return layer.msg(res.message)
+  //     }
+  //     layer.msg("注册成功，请登录！")
+  //     $("#link_login").click()
+  //   })
+  // })
+
+
   // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
+  $('#form_login').submit(function (e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
+      url: 'http://ajax.frontend.itheima.net/api/login',
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
